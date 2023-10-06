cf8d76ae88cb0b77802e966672bf9184e7305286
Ghtts06 <106259283+Ghtts06@users.noreply.github.com>
2023-10-06 11:49:23 -0300

    function

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-document.querySelector('.tecla_pom');
-document.querySelector('#som_recla_pom').play();
+function tocaSomPom () {
+    document.querySelector('#som_recla_pom').play();
+}
+
+document.querySelector(.'tecla_pom');
